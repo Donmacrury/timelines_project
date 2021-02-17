83b98161216acffd07bf79faa5adc169a1f301a2
Donald <donaldmacrury88@gmail.com>
2021-02-17 10:04:52 +0000

    initial commit

---
+++ server/timelines/src/main/java/com/codeclan/timelines/TimelinesApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.timelines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimelinesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimelinesApplication.class, args);
+	}
+
+}
---
+++ server/timelines/src/test/java/com/codeclan/timelines/TimelinesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeclan.timelines;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimelinesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
