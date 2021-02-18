cb0a40f83fa38b684ef91cc2b2523c34c5aeb812
Donald <donaldmacrury88@gmail.com>
2021-02-18 17:43:32 +0000

    simple map added

--- client/timelines_front/src/components/MapComponent.js
+++ client/timelines_front/src/components/MapComponent.js
@@ -1,5 +1,7 @@
 import Person from "./Person";
 import Event from "./Events";
+import L from 'leaflet';
+import { MapContainer, TileLayer, Marker, Popup} from 'react-leaflet';
 
 const MapComponent = ({events}) => {
 
@@ -13,9 +15,22 @@ const MapComponent = ({events}) => {
         });
     
         return (
+            <>
+            <MapContainer id = 'mapid'center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+            <TileLayer
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={[51.505, -0.09]}>
+                <Popup>
+                A pretty CSS3 popup. <br /> Easily customizable.
+                </Popup>
+            </Marker>
+            </MapContainer>
             <section>
                 {eventNodes}
             </section>
+            </>
         )
 
  }
