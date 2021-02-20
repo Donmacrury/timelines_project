f28b6d4d77ad8d0053f5f519935ea264ed60ad72
Donald <donaldmacrury88@gmail.com>
2021-02-20 13:15:02 +0000

    some initial plans for vertical timeline page

---
+++ client/timelines_front/src/components/TimelinePage.js
@@ -0,0 +1,17 @@
+import Event from './Event';
+
+// TODO: npm i react-verticle_timeline
+// 
+
+const TimelinePage = ({event}) => {
+    
+    cosnt [timelineEvents, setTimelineEvents] = useState(null)
+
+
+
+    return (
+        <>
+
+        </>
+    )
+}
