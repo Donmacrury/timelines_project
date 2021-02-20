c26c7cab98b591c555b0c5846ab6e4561c14cc86
Donald <donaldmacrury88@gmail.com>
2021-02-20 13:25:41 +0000

    added event arrangement for timeline

--- client/timelines_front/src/components/TimelinePage.js
+++ client/timelines_front/src/components/TimelinePage.js
@@ -8,19 +8,27 @@ const TimelinePage = ({event}) => {
     
     cosnt [timelineEvents, setTimelineEvents] = useState(null)
 
-
+    // TODO styles for battle event and non-battle-evetn, can be done in TimelinePage.css
 
     return (
         <>
             {/* <VerticalTimeline>
             {timelineEvents.map(event => {
+                let isBattleIcon - event.type ===="work";
+
                 return(
                     <VerticalTimelineElement
                     ket={event.key}
                     date={event.date}
                     dateClassName="date"
-                    // iconStyle={}
-                )
+                    iconStyle={isBattleIcon ? battleIconStyle : non-battleIconStyle}
+                    icon={isBattleIcon ? <BattleIcon/> :<non-battleIcon/>}
+                    >
+                    <h3 className="eventName">{event.name}</h3>
+                    <h5 className="eventLocation">{event.location}</h5>
+                    
+                    </VerticleTimelineElement>
+                );
             })}
             </VerticalTimeline>*/}
         </>
