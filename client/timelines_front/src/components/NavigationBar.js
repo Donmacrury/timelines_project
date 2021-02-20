4ab19d67bb982cf2cd3d773d259abdc52c0528b3
Donald <donaldmacrury88@gmail.com>
2021-02-20 13:35:53 +0000

    import Link and useHistory, initial setUp of component

---
+++ client/timelines_front/src/components/NavigationBar.js
@@ -0,0 +1,14 @@
+import {Link, useHistory} from 'react-router-dom';
+
+const NavBar = () => {
+    const history = useHistory();
+
+    const back = () => {
+        history.goBack();
+    }
+    return (
+        <nav class="navbar">
+
+        </nav>
+    )
+}
