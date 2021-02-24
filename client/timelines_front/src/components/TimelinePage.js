a6743e959f8f6601e0a869059f706af038fd2a4f
Donald <donaldmacrury88@gmail.com>
2021-02-24 11:43:34 +0000

    worked out location is a problem for the vertical timeline

--- client/timelines_front/src/components/TimelinePage.js
+++ client/timelines_front/src/components/TimelinePage.js
@@ -9,21 +9,22 @@ const TimelinePage = ({events}) => {
 
     return (
         <>
+        <h1>Timeline</h1>
             <VerticalTimeline>
             {events.map(event => {
-                // let isBattleIcon - event.type ===="work";
+                // let isBattleIcon = event.type ===="work";
                 // conditional logic for hasPersonList to see if the event has a list of person to then
                 // make the list of people button show up
                 return(
                     <VerticalTimelineElement
-                        key={event.id}
+                        key={event.key}
                         date={event.date}
                         dateClassName="date"
                         // iconStyle={isBattleIcon ? battleIconStyle : non-battleIconStyle}
                         // icon={isBattleIcon ? <BattleIcon/> :<non-battleIcon/>}
                     >
-                        <h3 className="eventName">{event.name}</h3>
-                        <h5 className="eventLocation">{event.location}</h5>
+                        <h3 className="vertical-timeline-element-title">{event.name}</h3>
+                        {/* <h5 className="vertical-timeline-element-subtitle">{event.location}</h5> */}
                         <p id="description">{event.description}</p>
                         <button>People</button> 
                         {/* this should have a popup for a list of people at the event */}
